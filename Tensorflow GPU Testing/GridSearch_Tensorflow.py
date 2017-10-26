205a5a5968babb433fb5724d9fc9373dc490be25
Steve Fischer <sfischer@ventera.com>
2017-10-26 09:06:05 -0400

    As far as I can go using skflow--so many commands are deprecated. Going to start using native TF.

--- Tensorflow GPU Testing/GridSearch_Tensorflow.py
+++ Tensorflow GPU Testing/GridSearch_Tensorflow.py
@@ -2,55 +2,86 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import os
-import random
+import datetime
 
+import matplotlib.pyplot as plt
 import pandas
-from tensorflow.contrib.learn.python import learn
-from tensorflow.python.platform import test
+import skflow
+from pylab import savefig
+from sklearn import metrics
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
 
-HAS_SKLEARN = os.environ.get('TENSORFLOW_SKLEARN', False)
-if HAS_SKLEARN:
-    try:
-        # pylint: disable=g-import-not-at-top
-        from sklearn import datasets
-        from sklearn import GridSearchCV
-        from sklearn.metrics import accuracy_score
-    except ImportError:
-        HAS_SKLEARN = False
+# Load the data set
+df = pandas.read_csv("ml_house_data_set.csv")
 
+# Remove the fields from the data set that we don't want to include in our model
+del df['house_number']
+del df['unit_number']
+del df['street_name']
+del df['zip_code']
 
-    class GridSearchTest(test.TestCase):
-        """Grid search tests."""
+# Replace categorical data with one-hot encoded data
+features_df = pandas.get_dummies(df, columns=['garage_type', 'city'])
+del features_df['sale_price']
 
-        def testIrisDNN(self):
-            if HAS_SKLEARN:
-                random.seed(42)
-                iris = datasets.load_iris()
-                feature_columns = learn.infer_real_valued_columns_from_input(iris.data)
+X = features_df.as_matrix()
+y = df['sale_price'].as_matrix()
 
-                ###STEVE
-                # Load the data set
-                df = pandas.read_csv("ml_house_data_set.csv")
-                # Replace categorical data with one-hot encoded data
-                features_df = pandas.get_dummies(df, columns=['garage_type', 'city'])
-                del features_df['sale_price']
+# Split the data set in a training set (70%) and a test set (30%)
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
+scaler = preprocessing.StandardScaler()
+X_train = scaler.fit_transform(X_train)
 
-                X = features_df.as_matrix()
-                y = df['sale_price'].as_matrix()
-                ###STEVE
+# # Create the model
+# model = ensemble.GradientBoostingRegressor()
+#
+# # Parameters we want to try
+# param_grid = {
+#     'n_estimators': [500, 1000, 3000],
+#     'max_depth': [4, 6],
+#     'min_samples_leaf': [3, 5, 9, 17],
+#     'learning_rate': [0.1, 0.05, 0.02, 0.01],
+#     'max_features': [1.0, 0.3, 0.1],
+#     'loss': ['ls', 'lad', 'huber']
+# }
 
-                classifier = learn.DNNClassifier(
-                    feature_columns=feature_columns,
-                    hidden_units=[10, 20, 10],
-                    n_classes=3)
-                grid_search = GridSearchCV(
-                    classifier, {'hidden_units': [[5, 5], [10, 10]]},
-                    scoring='accuracy',
-                    fit_params={'steps': [50]})
-                grid_search.fit(iris.data, iris.target)
-                score = accuracy_score(iris.target, grid_search.predict(iris.data))
-                self.assertGreater(score, 0.5, 'Failed with score = {0}'.format(score))
+# regressor = skflow.TensorFlowDNNRegressor(hidden_units=[10, 10, 10],
+#     steps=20000, learning_rate=0.01, batch_size=13)
 
-if __name__ == '__main__':
-    test.main()
+# regressor = SVR(kernel='rbf', C=1000, gamma='auto')
+
+regressor = skflow.TensorFlowLinearRegressor(steps=2000, learning_rate=0.01, batch_size=13)
+
+ts_a = datetime.datetime.now()
+
+# Train and Predict
+
+regressor.fit(X_train, y_train)
+score = metrics.mean_squared_error(regressor.predict(scaler.transform(X_test)), y_test)
+X_ty = regressor.predict(X_train)
+score1 = metrics.mean_squared_error(X_ty, y_train)
+
+print('Test MSE: {0:f}'.format(score))
+print('Train MSE: {0:f}'.format(score1))
+
+# get end timestamp
+ts_b = datetime.datetime.now()
+
+delta = ts_b - ts_a
+
+print("Total GPU Runtime Duration was " + repr(int(delta.min)) + " minutes.")
+
+print('Test MSE: {0:f}'.format(score))
+print('Train MSE: {0:f}'.format(score1))
+########################################################
+# Look at the results
+fig, ax = plt.subplots()
+ax.scatter(y_train, X_ty)
+ax.plot([y_train.min(), y_train.max()], [y_train.min(), y_train.max()], 'k--', lw=4)
+ax.set_xlabel('Measured')
+ax.set_ylabel('Predicted')
+plt.show()
+
+savefig('result.png')
